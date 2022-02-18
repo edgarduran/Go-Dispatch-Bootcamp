a46ab26d1b4676db10836e304fbb719b2c75c59b
Edgar Duran <edgar.duran@dispatchhealth.com>
2022-02-18 09:46:20 -0700

    parse and return csv

--- service/vehicle.go
+++ service/vehicle.go
@@ -1,15 +1,44 @@
 package service
 
 import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
+
 	"github.com/edgarduran/Go-Dispatch-Bootcamp/models"
 )
 
-var vehicles = []models.Vehicle{
-	{Id: 1, Make: "ford", Model: "mustang"},
-	{Id: 2, Make: "kia", Model: "sorento"},
-}
-
 func GetAllVehicles() ([]models.Vehicle, error) {
-	// TODO: replace with csv data
-	return vehicles, nil
+	f, err := os.Open("vehicles.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var vehicleList []models.Vehicle
+	for _, vehicle := range data {
+		var rec models.Vehicle
+		for j, field := range vehicle {
+			if j == 0 {
+				id, _ := strconv.ParseInt(field, 10, 64)
+				rec.Id = id
+			} else if j == 1 {
+				rec.Make = field
+			} else if j == 2 {
+				rec.Model = field
+			}
+		}
+
+		vehicleList = append(vehicleList, rec)
+	}
+
+	return vehicleList, nil
 }
